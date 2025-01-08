56cf65431ef881fadcade6a842bfc4fcdce4c9fe
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-08 13:50:10 -0300

    componente standalone false, passando a declracao corretamente

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
@@ -9,12 +9,13 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 @NgModule({
   declarations: [
     AppComponent,
+    MeuPrimeiroComponent,
     MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MeuPrimeiroComponent
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
