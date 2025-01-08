3747441f8058615efa2e5387e2c203cf16ef22a6
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-08 13:49:31 -0300

    colocando o component com standalone como false

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'meu-primeiro-component',
+    standalone: false,
     template: `
         <p>Meu primeiro component com Angular 2!</p>
     `
