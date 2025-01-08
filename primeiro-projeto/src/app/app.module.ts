7926eea4c2c7e1c214f5dddd2f69b452e4bf1fba
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-08 14:18:06 -0300

    aula 5

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  standalone: false,
+  
+  templateUrl: './cursos.component.html',
+  styleUrl: './cursos.component.css'
+})
+export class CursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
