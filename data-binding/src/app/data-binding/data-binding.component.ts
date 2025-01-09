dc523fcc28b4bad5afa0ede8a8c3f73fe41de650
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-09 09:13:44 -0300

    databinding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  standalone: false,
+  
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+  url: any = 'https://www.linkedin.com/in/fabiomats/';
+  cursoAngular: boolean = true;
+  urlImagem = 'http://lorempixel.com.br/500/400/?40'
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+}
