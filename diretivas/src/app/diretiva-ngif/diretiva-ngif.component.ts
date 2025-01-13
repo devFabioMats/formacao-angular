e61f67fd4e3c45f9848c6bfd8dc0247a41fedc9e
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-13 09:16:56 -0300

    criando componente ngif

---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  imports: [],
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrl: './diretiva-ngif.component.css'
+})
+export class DiretivaNgifComponent {
+
+}
