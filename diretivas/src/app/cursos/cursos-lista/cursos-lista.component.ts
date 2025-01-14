e8edcf6705930f2dcb57799884a5ebd7a8df8904
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-14 11:31:11 -0300

    criacao do componente e roteamento de cursos

---
+++ diretivas/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-lista',
+  standalone: false,
+  
+  templateUrl: './cursos-lista.component.html',
+  styleUrl: './cursos-lista.component.scss'
+})
+export class CursosListaComponent {
+
+}
