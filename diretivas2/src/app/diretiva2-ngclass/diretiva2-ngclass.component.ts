e663f0b11339ba671edf6f8dea6966a9586ee1d2
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-15 10:06:03 -0300

    usando property binding e NGCLASS

--- diretivas2/src/app/diretiva2-ngclass/diretiva2-ngclass.component.ts
+++ diretivas2/src/app/diretiva2-ngclass/diretiva2-ngclass.component.ts
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class Diretiva2NgclassComponent {
 
+  meuFavorito: boolean = false;
+
+  onClick() {
+    this.meuFavorito = !this.meuFavorito;
+  }
 }
