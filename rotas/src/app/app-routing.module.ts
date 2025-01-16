5306046544e481fc8158318ca543798176e50f7b
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-16 11:06:26 -0300

    rotas basicas funcionadno

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
+const APP_ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'login', component: LoginComponent },
+];
+
+ @NgModule({
+  declarations: [], 
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
-})
-export class AppRoutingModule { }
+ })
+ export class AppRoutingModule {}
