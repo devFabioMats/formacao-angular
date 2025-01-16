8ce911ad583251e5a58ba8c9719d4cd236a1b466
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-16 08:50:07 -0300

    usando servicos

--- servicos/src/app/cursos/cursos.component.ts
+++ servicos/src/app/cursos/cursos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   standalone: false,
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './cursos.component.css'
 })
 export class CursosComponent {
+  cursos: string[] = [];
+  CursosService: CursosService;
 
+  constructor() {
+    this.CursosService = new CursosService();
+    this.cursos = this.CursosService.getCursos();
+  }
 }
