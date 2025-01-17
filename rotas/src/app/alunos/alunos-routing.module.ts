d864e71781a36b6b20cd1c4cd8e2854765d13627
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-17 10:36:35 -0300

    rotas filhas feitas

--- rotas/src/app/alunos/alunos-routing.module.ts
+++ rotas/src/app/alunos/alunos-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-const alunosRoutes: any[] = [];
+import { AlunosComponent } from './alunos.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+
+
+const alunosRoutes: Routes = [
+  { path: 'alunos', component: AlunosComponent, children: [
+      { path: 'novo', component: AlunoFormComponent },
+      { path: ':id', component: AlunoDetalheComponent },
+      { path: ':id/editar', component: AlunoFormComponent },
+    ]}
+];
 
 @NgModule({
-    imports: [ RouterModule.forChild(alunosRoutes) ],
+    imports: [ CommonModule ,RouterModule.forChild(alunosRoutes) ],
     exports: [ RouterModule ],
     declarations: [],
     bootstrap: [],
