3fd78b75d01eb3c82f89c2ba6edcc913c53a6e8c
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-01-22 14:36:40 -0300

    instanciando e configurnado componenete de mostrar erro campo control erro

--- forms/src/app/campo-control-erro/campo-control-erro.component.ts
+++ forms/src/app/campo-control-erro/campo-control-erro.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-campo-control-erro',
   standalone: false,
-  
+
   templateUrl: './campo-control-erro.component.html',
-  styleUrl: './campo-control-erro.component.css'
+  styleUrl: './campo-control-erro.component.css',
 })
 export class CampoControlErroComponent {
-
+  @Input() mostrarErro: boolean = false;
+  @Input() mensagemErro: string = '';
 }
--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -5,11 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { FormDebugComponent } from '../form-debug/form-debug.component';
 import { DataFormComponent } from './data-form.component';
-
-
+import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
 
 @NgModule({
-  declarations: [DataFormComponent, FormDebugComponent ],
+  declarations: [
+    DataFormComponent,
+    FormDebugComponent,
+    CampoControlErroComponent,
+  ],
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
 })
 export class DataFormModule {}
